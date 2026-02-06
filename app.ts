52246bd510a2f6c7afe30ca664e797a36e578886
Vinay <53383503+vikuviku6666@users.noreply.github.com>
2026-02-06 10:00:08 +0100

    added req

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { type Request, type Response, type NextFunction } from "express";
 
 import todoRoutes from './routes/todo.js'
 
